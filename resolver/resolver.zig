8f183c6bf4b3bf56ae83f38b1cf6c998764dcd5c
arshia-senapt <119294140+arshia-senapt@users.noreply.github.com>
2025-08-14 14:30:33 +0100

    kermit

--- resolver/resolver.zig
+++ resolver/resolver.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 
-pub fn resolveIPv4(name: []const u8) !std.net.Ip4Address {
+fn ipv4ToString(addr: std.net.Address) *const [4]u8{
+    const ipv4 = addr.in;
+    const bytes = @as(*const [4]u8, @ptrCast(&ipv4.sa.addr));
+    return bytes;
+}
+
+pub fn resolveIPv4(name: []const u8) !*const [4]u8 {
     if (std.net.Address.parseIp4(name, 0)) |addr| {
-        return addr.in;
+        const ip4 = addr.in;
+        const bytes = @as(*const [4]u8, @ptrCast(&ip4.sa.addr));
+        return bytes;
     } else |_| {}
 
     var list = try std.net.getAddressList(std.heap.page_allocator, name, 0);
@@ -10,7 +18,9 @@ pub fn resolveIPv4(name: []const u8) !std.net.Ip4Address {
 
     for (list.addrs) |addr| {
         if (addr.any.family == std.posix.AF.INET) {
-            return addr.in;
+            const ip4 = addr.in;
+            const bytes = @as(*const [4]u8, @ptrCast(&ip4.sa.addr));
+            return bytes;
         }
     }
     return error.NoIPv4AddressFound;
